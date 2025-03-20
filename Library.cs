14739f058dd816a3fad083a767d2d69b9cc5da9a
top <Top@8-7>
2025-03-20 15:21:42 +0500

    Добавьте файлы проекта.

---
+++ Library.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Library<T> : IMediaManager<T> where T : Media
+    {
+        public List<T> MediaList { get { return _list; } }
+
+        private Dictionary<string, T>  _dict = new Dictionary<string, T>();
+
+        private List<T> _list = new List<T>();
+
+        public void Add(T item)
+        {
+            try
+            {
+                MediaList.Add(item);
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("Item already exist");
+            }
+            
+        }
+        public bool Remove(string title)
+        {
+            if (title == null) throw new ArgumentNullException("argument can't be null");
+            foreach (var item in MediaList)
+            {
+                if (item.Title == title)
+                {
+                    MediaList.Remove(item);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public T FindByTitle(string title)
+        {
+            if (title == null) throw new ArgumentNullException("argument can't be null");
+            foreach (var item in MediaList)
+            {
+                if (item.Title == title)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        public IEnumerable<T> FilterByYear(int year)
+        {
+            if (year == null) throw new ArgumentNullException("argument can't be null");
+            List<T> li = new List<T>();
+
+            foreach (var item in MediaList)
+            {
+                if (item.YearPublished == year)
+                {
+                    li.Add(item);
+                }
+            }
+            return li;
+        }
+
+        public IEnumerable<T> GetAllAvailable()
+        {
+            List<T> li = new List<T>();
+            foreach (var item in MediaList)
+            {
+                if (item.IsAvailable)
+                {
+                    li.Add(item);
+                }
+            }
+            return li;
+        }
+        public IEnumerable<T> GetAllUnavailable()
+        {
+            var query = from item in MediaList where !item.IsAvailable select item;
+            return query.ToList();
+        }
+
+        public void PrintAll()
+        {
+            foreach (var item in MediaList)
+            {
+                Console.WriteLine($"Title: {item.Title},\nAvailable: {item.IsAvailable},\nYear published: {item.YearPublished}\n\n");
+            }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Library<Media> lib = new Library<Media>();
+            lib.MediaList.Add(new Media() { Author = "Author1", Title = "Title1", YearPublished = 1987, IsAvailable = true });
+            lib.MediaList.Add(new Media() { Author = "Author2", Title = "Title2", YearPublished = 2005, IsAvailable = false });
+            lib.MediaList.Add(new Media() { Author = "Author2", Title = "Title3", YearPublished = 2008, IsAvailable = true });
+            lib.MediaList.Add(new Media() { Author = "Author3", Title = "Title3", YearPublished = 2008, IsAvailable = false });
+            lib.MediaList.Add(new Media() { Author = "Author3", Title = "Title8", YearPublished = 1995, IsAvailable = true });
+            lib.MediaList.Add(new Media() { Author = "Author4", Title = "Title5", YearPublished = 2015, IsAvailable = false });
+            lib.MediaList.Add(new Media() { Author = "Author5", Title = "Title6", YearPublished = 2002, IsAvailable = false });
+           
+            lib.PrintAll();
+        }
+    }
+}
